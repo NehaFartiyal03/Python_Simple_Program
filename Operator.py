50b8ba8d72a5148a21fd7389460eb829311f1dee
Neha Fartiyal <1000024339@dit.edu.in>
2025-06-03 13:22:06 +0530

    Create Operator.py

---
+++ Operator.py
@@ -0,0 +1,49 @@
+#arithmetic operators (+,-,*,/,%,**)
+a = 56
+b = 2
+#sum = a+b
+#product = a*b
+#sub = a-b
+#div = a/b
+#print(sum)
+#print(product)
+#print(sub)
+#print(div)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b) #remainder
+print(a ** b) #a^b
+
+#relational operators (==, !=, >= ,<=)
+a = 50
+b = 60
+print(a == b)
+print(a !=b)
+print(a>=b)
+print(a<=b)
+
+#Assignment Operators(=,+=,-=,*=,/=,%=,**=)
+num = 10
+#num = num +10 #10+10=20
+#num += 10
+#num -= 10
+#num *= 10
+#num /= 10
+#print("num :", num)
+
+#logical operator(NOT,OR, AND)
+#NOT
+a=70
+b=50
+print(not True)
+print(not (a<b))
+
+#AND
+val1 = True
+val2 = False
+print("AND operator:", val1 and val2) # jab dono value true hogi tbhi true hoga 
+#OR
+print("OR operator:", val1 or val2)
+print("OR operator:", (a == b) or (a > b))
