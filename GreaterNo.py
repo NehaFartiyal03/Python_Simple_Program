785b279ee30daf7776958bc23674254426936b09
Neha Fartiyal <1000024339@dit.edu.in>
2025-06-03 13:25:59 +0530

    Create GreaterNo.py

---
+++ GreaterNo.py
@@ -0,0 +1,10 @@
+#WAP to input 2 int numbers, a and b. Print True if a is greater than or equal to b. if not print False.
+#simple format
+a = 30
+b = 70
+print(a>=b)
+
+a = float(input("enter first no:"))
+b = float (input("enter second no:"))
+
+print(a>=b)
