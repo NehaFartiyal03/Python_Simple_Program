4aceb613e495a8b594c5330d176e0deefbb11cbf
Neha Fartiyal <1000024339@dit.edu.in>
2025-06-03 13:24:40 +0530

    Create InputUser(SSPDM).py

---
+++ InputUser(SSPDM).py
@@ -0,0 +1,10 @@
+# Write a program to input 2 numbers & print their sum, product, div, sub, module.
+
+first =int(input("enter first:"))
+second =int(input("enter second:"))
+
+print("sum=", first + second)
+print("sub=", first - second)
+print("Product=", first * second)
+print("div=", first / second)
+print("mod=", first % second)
