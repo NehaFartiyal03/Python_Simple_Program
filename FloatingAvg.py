68ebec2b66b6d4f27b980a66f717c6cdc7cc1daf
Neha Fartiyal <1000024339@dit.edu.in>
2025-06-03 13:25:36 +0530

    Create FloatingAvg.py

---
+++ FloatingAvg.py
@@ -0,0 +1,7 @@
+# WAP to input 2 floating point numbers & print their average.
+
+a = float(input("enter your first no:"))
+b = float(input("enter your second no:"))
+#avg = (a+b)/2
+
+print("avg=", (a+b)/2)
