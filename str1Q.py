c20ed42a817035ddffd9712c8cc1073452f648e7
Neha Fartiyal <1000024339@dit.edu.in>
2025-06-04 12:56:40 +0530

    Create str1Q.py

---
+++ str1Q.py
@@ -0,0 +1,9 @@
+#WAP to input user's first name & print its length.
+
+name = input("enter Name:")
+#len = len(name)
+print("length of your name:",len(name))
+
+#WAP to find the occurrence of '$' in a String.
+str = "Hy $Iam the $ symbol $99.89"
+print(str.count("$")) #3
