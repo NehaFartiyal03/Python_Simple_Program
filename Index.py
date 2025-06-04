bd5a3bca00ef3dae3da454e238d30f9dd914e2b4
Neha Fartiyal <1000024339@dit.edu.in>
2025-06-04 12:57:06 +0530

    Create Index.py

---
+++ Index.py
@@ -0,0 +1,18 @@
+# Indexing
+#str = "My name is Nidhi"
+#print(str[3]) # n
+
+ #type 2
+#str1 = "nidhi"
+#ch = str1[4]
+#print(str1[4]) #i
+#print(str1[2:4])#slicing
+
+#String functions
+
+str ="i am mca student for dit university dehradun"
+print(str.endswith("dun")) #true
+print(str.capitalize()) # I am mca student for dit university dehradun
+print(str.replace("a","p"))
+print(str.find("d"))
+print(str.count("a"))
