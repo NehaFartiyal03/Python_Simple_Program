f3a3f03b6a4cf7e45b255a51ca58647abfffd854
Neha Fartiyal <1000024339@dit.edu.in>
2025-06-04 12:55:59 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,20 @@
+#str1 = "my name is nidhi."
+#str2 = 'my age is 21'
+
+#str3 ='''My course name is MCA'''
+#print(str3) # My course name is MCA
+
+#str4 ='my name is neha and nidhi \n age is 21 year old'
+#print(str4) #my name is neha and nidhi
+            # age is 21 year old
+
+str5 = "Welcome Nidhi"
+len5 = len(str5)
+print(len5) #13
+str6 = "Nidhi is a good girl"
+len6 = len(str6)
+print(len6) #20
+final_str = str5 + "" +str6
+
+print(len(final_str)) # 33
+
