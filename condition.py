8d8ac2e7ac1650015fc7a6d0c5d6152d15fafbaf
Neha Fartiyal <1000024339@dit.edu.in>
2025-06-04 12:57:44 +0530

    Create condition.py

---
+++ condition.py
@@ -0,0 +1,35 @@
+#age = 12
+
+#if(age >= 18):
+ #   print("you eligiliable for vote:")
+ #   print("you can drive the car:")
+#else:
+ #   print("you cant eligible to vote:")
+
+
+
+#light = "pink"
+
+#if(light == "red"):
+ #   print("stop:")
+#elif(light == "green"):
+ #   print("go:")
+#elif(light == "yellow"):
+    #print("wait:")
+#else:
+ #   print("light is broken")
+
+    #print("end of code")
+
+marks = int(input("enter student marks:"))
+
+if(marks >= 90):
+    print("grade A")
+elif(marks >= 80 and marks < 90):
+    print("grade B")
+elif(marks >=70 and marks < 80):
+    print("grade C")
+else:
+    print("grade D")
+
+    print("grade of the student->", grade)
