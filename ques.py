1c7e32f09b6cb5816fcb8e82a227c1baeecffeb8
Neha Fartiyal <1000024339@dit.edu.in>
2025-06-05 11:20:58 +0530

    Create ques.py

---
+++ ques.py
@@ -0,0 +1,8 @@
+#WAP to count the number of students with the "A" grade in the following typle.
+#Wap the above values in a list & sort them from "A" to "D".
+grade =["c","D","A","B","B","A"]
+grade.sort()
+grade.append("E")
+print(grade)
+
+
